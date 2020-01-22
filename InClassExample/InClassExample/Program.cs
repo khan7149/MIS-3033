9f25c32163b29afff9aa25120a3a8ff6a051a514
khan7149 <khan7149@PCB-LAB360-07>
2020-01-22 11:27:07 -0600

    Finsihed

--- InClassExample/InClassExample/Program.cs
+++ InClassExample/InClassExample/Program.cs
@@ -11,8 +11,33 @@ namespace InClassExample
 {
     class Program
     {
+        /// <summary>
+        /// Sums up two numbers and provides the result
+        /// </summary>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        
+
+        static double Add(double val1, double val2)
+        {
+            double sum = val1 + val2;
+            return sum;
+        }
+
         static void Main(string[] args)
         {
+            double result = Add(5,6);
+
+            Console.WriteLine($"5 + 6 = {result.ToString("N2")}");
+            Console.ReadLine();
         }
+
+       
+
+        
     }
+    
 }
+
+    
