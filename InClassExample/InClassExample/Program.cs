27a7f2617de9a019f0a80f5a13023c9f9e30ddc9
khan7149 <khan7149@PCB-LAB360-07>
2020-01-22 11:09:52 -0600

    IDK

---
+++ InClassExample/InClassExample/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Faraz Khan
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
